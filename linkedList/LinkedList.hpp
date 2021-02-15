cbe423e1e69b1663eb215edc81fd4e7c234e2c75
Aubrey Stephens <aubreystephens03@gmail.com>
2021-02-14 23:13:00 -0600

    add removeBack definition

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -114,9 +114,22 @@ bool LinkedList<T>::removeBack()
 	Node<T>* secondintoLast = nullptr;
 	bool isRemoved = false;
 
-	/** TODO 
-		Fix this method
-	*/
+	if(isEmpty())
+    {
+        return (isRemoved);
+    }
+    else
+    {
+        while(lastNode->getNext() != nullptr)
+        {
+            secondintoLast = lastNode;
+            lastNode = lastNode->getNext();
+        }
+        secondintoLast->setNext(nullptr);
+        delete lastNode;
+        m_size--;
+        isRemoved = true;
+    }
 
 	return(isRemoved);
 }	
